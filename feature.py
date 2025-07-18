1dd4ebd4d621fb67e947ec01d33d3fa0ea272857
SSKRCP <sonali.rcp@gmail.com>
2025-07-18 15:00:49 +0800

    added feature.py with a function to add numbers

---
+++ feature.py
@@ -0,0 +1,11 @@
+# Function to add two numbers
+def fn_add_numbers(num1, num2):
+    return num1 + num2
+
+# define variables
+n1 = 10
+n2 = 20
+
+# Make a call to function and print the result
+result = fn_add_numbers(n1, n2)
+print (result)
